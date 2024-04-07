b74780cd8ac013ebf839c183ba193cb5bf6fc492
Rohan-mgr <rohan.magar.415@gmail.com>
2024-04-07 14:13:42 +0545

    made loader color dynamic

--- lib/widgets/loader.dart
+++ lib/widgets/loader.dart
@@ -2,15 +2,18 @@ import 'package:flutter/material.dart';
 
 class Loader extends StatelessWidget {
   final double size;
+  final Color color;
 
-  const Loader({super.key, this.size = 25.0});
+  const Loader({super.key, this.size = 25.0, this.color = Colors.white});
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: size, // Adjust height as needed
       width: size,
-      child: CircularProgressIndicator(),
+      child: CircularProgressIndicator(
+        color: color,
+      ),
     );
   }
 }
