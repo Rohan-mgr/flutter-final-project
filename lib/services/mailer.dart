10792e77de181ed0070d0a565f25950c8eb225bf
Mingmar Gyalzen Sherpa <mingmagsherpa6@gmail.com>
2024-04-08 16:06:48 +0545

    handled-mail-error-and-forgot-password-error

--- lib/services/mailer.dart
+++ lib/services/mailer.dart
@@ -5,38 +5,39 @@ import 'package:mailer/mailer.dart';
 import 'package:mailer/smtp_server.dart';
 import 'package:intl/intl.dart';
 
-void sendMail({String? recipientEmail, String? recipientName}) async {
+Future<bool> sendMail({String? recipientEmail, String? recipientName}) async {
   String username = dotenv.env['APP_EMAIL']!;
   String password = dotenv.env['APP_EMAIL_PASSWORD']!;
-
-  final smtpServer = gmail(username, password);
-
-  // Format the current date and time
-  String formattedDateTime =
-      DateFormat('yyyy-MM-dd HH:mm a', 'en_US').format(DateTime.now());
-
-  String otp = generateOTP();
-
-  String htmlContent =
-      await rootBundle.loadString('assets/email_template.html');
-
-  htmlContent = htmlContent.replaceAll('{recipientName}', recipientName!);
-  htmlContent = htmlContent.replaceAll('{otp}', otp);
-  htmlContent = htmlContent.replaceAll('{date}', formattedDateTime);
-
-  // Create our message.
-  final message = Message()
-    ..from = Address(username, 'RPM Group')
-    ..recipients.add(recipientEmail)
-    // ..ccRecipients.addAll(['destCc1@example.com', 'destCc2@example.com'])
-    // ..bccRecipients.add(Address('bccAddress@example.com'))
-    ..subject = 'Reset Your Password - ${formattedDateTime}'
-    ..html = htmlContent;
-
   try {
+    final smtpServer = gmail(username, password);
+
+    // Format the current date and time
+    String formattedDateTime =
+        DateFormat('yyyy-MM-dd HH:mm a', 'en_US').format(DateTime.now());
+
+    String otp = generateOTP();
+
+    String htmlContent =
+        await rootBundle.loadString('assets/email_template.html');
+
+    htmlContent = htmlContent.replaceAll('{recipientName}', recipientName!);
+    htmlContent = htmlContent.replaceAll('{otp}', otp);
+    htmlContent = htmlContent.replaceAll('{date}', formattedDateTime);
+
+    // Create our message.
+    final message = Message()
+      ..from = Address(username, 'RPM Group')
+      ..recipients.add(recipientEmail)
+      // ..ccRecipients.addAll(['destCc1@example.com', 'destCc2@example.com'])
+      // ..bccRecipients.add(Address('bccAddress@example.com'))
+      ..subject = 'Reset Your Password - ${formattedDateTime}'
+      ..html = htmlContent;
+
     final sendReport = await send(message, smtpServer);
     print('Mail send successfull' + sendReport.toString());
-  } on MailerException catch (error) {
+    return true;
+  } catch (error) {
     print("Error sending mail: $error");
+    return false;
   }
 }
