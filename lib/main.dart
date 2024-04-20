9f897b65bcdd074b2f0448615f1a38416de3ca39
Rohan-mgr <rohan.magar.415@gmail.com>
2024-04-20 22:10:21 +0545

    removed unwanted code

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,11 @@ import 'package:flutter_final_project/screens/index.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-// import 'package:firebase_app_check/firebase_app_check.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  // await FirebaseAppCheck.instance.activate(
-  //   androidProvider: AndroidProvider.debug,
-  // );
   runApp(MyApp());
 }
 
